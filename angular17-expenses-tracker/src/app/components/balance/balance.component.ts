1bc46aef9bd52b5163309531e5400083edeb576e
efrainlav <efrainlav@gmail.com>
2023-12-03 15:59:15 -0500

    Angular: Input basado en una interface. ELM

--- angular17-expenses-tracker/src/app/components/balance/balance.component.ts
+++ angular17-expenses-tracker/src/app/components/balance/balance.component.ts
@@ -12,12 +12,8 @@ import { Balance } from '../../models/balance.model';
   styleUrl: './balance.component.scss',
 })
 export class BalanceComponent {
-  @Input() title: string = 'Titulo';
-  @Input() balance: Balance = {
-    amount: 55_000,
-    income: 100_000,
-    expenses: 45_000,
-  };
+  @Input() title!: string;
+  @Input() balance!: Balance;
 
   // //Variables con tipo implícito
   // title = 'This is the balance component';
