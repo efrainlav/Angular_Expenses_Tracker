ee13879b56e97c6e6df6e20f73290b8779921936
efrainlav <efrainlav@gmail.com>
2023-12-03 15:53:37 -0500

    Angular: Definión y uso del atributo del componente basado en una interface - Balance -. ELM

--- angular17-expenses-tracker/src/app/components/balance/balance.component.ts
+++ angular17-expenses-tracker/src/app/components/balance/balance.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { Balance } from '../../models/balance.model';
+
 @Component({
   selector: 'app-balance',
   standalone: true,
@@ -10,7 +12,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './balance.component.scss',
 })
 export class BalanceComponent {
-  @Input() title: string = '';
+  @Input() title: string = 'Titulo';
+  @Input() balance: Balance = {
+    amount: 55_000,
+    income: 100_000,
+    expenses: 45_000,
+  };
 
   // //Variables con tipo implícito
   // title = 'This is the balance component';
