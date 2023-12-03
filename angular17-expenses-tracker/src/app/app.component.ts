085f2905b8ba78b056260a9d0d49fc6aaa18006a
efrainlav <efrainlav@gmail.com>
2023-12-03 15:16:35 -0500

    Angular: Creacion y uso del componente Balance. ELM

---
+++ angular17-expenses-tracker/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { BalanceComponent } from './components/balance/balance.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, BalanceComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Elm-Expense TRacker';
+}
